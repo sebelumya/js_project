039a3d95bbaa9fbdb4b43a7dac76a9aa3b75caa7
sebelumya <hamzahmuhajirin@gmail.com>
2021-03-16 18:33:25 +0700

    edited files

---
+++ domManipulation/button/button.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', function (e) {
+    e.target.style.background = 'blue';
+});
+
+//btn.addEventListener('click', () => {
+  //  alert("Hello World");
